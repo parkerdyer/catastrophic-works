957f7c5c21c41c52e2669df373d14a5f6f255bcb
Parker Dyer <dyer.lp@gmail.com>
2019-07-30 23:06:36 -0400

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
   siteMetadata: {
     title: 'Ghost',
     description: 'The professional publishing platform',
-    siteUrl: 'https://gatsby-casper.netlify.com', // full path to blog - no ending slash
+    siteUrl: 'https://www.catastrophic.works', // full path to blog - no ending slash
   },
   mapping: {
     'MarkdownRemark.frontmatter.author': 'AuthorYaml',
